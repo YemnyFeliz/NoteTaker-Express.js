e2d3853f6017db8d6414265d815f097c973324c9
Yemny <y.feliz1296@gmail.com>
2023-09-26 12:51:48 -0400

    Created to add new notes and delete notes in notes.js. Added wildcard in server.js to direct to home page

---
+++ routes/notes.js
@@ -0,0 +1,39 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, readAndDelete } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if ( title, text ) {
+        const newNotes = {
+            id: uuidv4(),
+            title: title,
+            text: text,
+        };
+
+        readAndAppend(newNotes, './db/db.json');
+        res.json('New note has been created successfully!');
+
+    } else {
+        res.errored('Something went wrong...');
+    }
+});
+
+notes.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    readAndDelete(id, './db/db.json', (err) => {
+        if (err) {
+            res.status(500).send('Could not delete note...');
+        }else {
+            res.status(200).send('Note has been deleted!');
+        }
+    });
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Wildcard route so when the path does not match it directs to indexx.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
