a5d8d4f58f0085675958fad36f78b671061e6b4e
Yemny <y.feliz1296@gmail.com>
2023-09-26 20:25:35 -0400

    small sintax fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(clog);
@@ -28,7 +28,7 @@ app.get('/notes', (req, res) =>
 // Wildcard route so when the path does not match it directs to index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+});
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
